9073d0b3b0dd5f182b3b83ca0ed3b2c71f5bc8eb
Victor Adebayo <156952024+TechVictorAdebayo@users.noreply.github.com>
2024-02-21 21:44:19 +0100

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,11 @@
+#Prompt the user for their name
+name = input("What is your name?")
+
+#prompt the user for their location
+age = input("How old are you?")
+
+#prompt the user for their location
+location = input("Where are you located?")
+
+#Display the user information
+print(f"Hello {name}, you are {age} years old and live in {location}")
